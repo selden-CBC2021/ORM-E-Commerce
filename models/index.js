fce15867b482eb5786d84dbb70aa3e0304e471ec
Drew Selden <seldend1@gmail.com>
2021-08-28 23:15:29 -0400

    added create/delete routes for Tag model

--- models/index.js
+++ models/index.js
@@ -8,21 +8,22 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey:"category_id",
   onDelete: "CASCADE"
-})
+});
 // Categories have many Products
 Category.hasMany(Product, {
   foreignKey:"category_id",
-})
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: "product_id",
-})
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: "tag_id",
-})
+  as: 'product_tags'
+});
 
 module.exports = {
   Product,
