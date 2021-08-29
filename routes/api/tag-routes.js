00225616f976fe961ed0565f67e34270a7129462
Drew Selden <seldend1@gmail.com>
2021-08-29 00:01:56 -0400

    added category create, read, destroy routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -13,10 +13,9 @@ router.get('/', async (req, res) => {
     res.status(500).json(err)
   }
 });
-
+// find a single tag by its `id`
+// be sure to include its associated Product data
 router.get('/:id', async (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.body.id, {
       include:[{model: Product, through: ProductTag, as: 'product_tags' }]
@@ -39,12 +38,13 @@ router.post('/', async (req, res) => {
     res.status(400).json(err);
   }
 });
-
+// update a tag's name by its `id` value
 router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
+  
 });
- // delete on tag by its `id` value
-router.delete('/:id', (req, res) => {
+
+// delete on tag by its `id` value
+router.delete('/:id', async (req, res) => {
   try {
     const tagData = await Tag.destroy({
       where: {
